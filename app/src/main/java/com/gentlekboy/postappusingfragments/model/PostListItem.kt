e5187ffa7afca5140753c718dd5ddc6de2e04aa9
gentlekboy <gentlekboy@gmail.com>
2021-09-20 01:45:39 +0100

    set up data class as entity

--- app/src/main/java/com/gentlekboy/postappusingfragments/model/PostListItem.kt
+++ app/src/main/java/com/gentlekboy/postappusingfragments/model/PostListItem.kt
@@ -1,6 +1,11 @@
 package com.gentlekboy.postappusingfragments.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "posts")
 data class PostListItem(
+    @PrimaryKey(autoGenerate = true)
     val body: String,
     val id: Int,
     val title: String,
