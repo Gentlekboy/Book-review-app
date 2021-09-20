8449b03725eab895c403b537e64d8ff8e84883d7
gentlekboy <gentlekboy@gmail.com>
2021-09-21 00:41:18 +0100

    created room database

--- app/src/main/java/com/gentlekboy/postappusingfragments/database/PostDatabase.kt
+++ app/src/main/java/com/gentlekboy/postappusingfragments/database/PostDatabase.kt
@@ -1,5 +1,26 @@
 package com.gentlekboy.postappusingfragments.database
 
-class PostDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.gentlekboy.postappusingfragments.model.PostListItem
 
+@Database(entities = [PostListItem::class], version = 1, exportSchema = false)
+abstract class PostDatabase: RoomDatabase() {
+    abstract fun getPostDao(): PostDao
+
+    companion object{
+        private var dbInstance: PostDatabase? = null
+
+        fun getDbInstance(context: Context): PostDatabase{
+            if (dbInstance == null){
+                dbInstance = Room.databaseBuilder(context.applicationContext, PostDatabase::class.java, "postDb")
+                    .allowMainThreadQueries()
+                    .build()
+            }
+
+            return dbInstance!!
+        }
+    }
 }
