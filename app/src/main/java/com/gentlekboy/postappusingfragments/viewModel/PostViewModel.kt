970cc50f8b725520e853bca683d3ff6265d01153
gentlekboy <gentlekboy@gmail.com>
2021-09-21 00:41:49 +0100

    created view model

---
+++ app/src/main/java/com/gentlekboy/postappusingfragments/viewModel/PostViewModel.kt
@@ -0,0 +1,19 @@
+package com.gentlekboy.postappusingfragments.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.gentlekboy.postappusingfragments.model.PostListItem
+import com.gentlekboy.postappusingfragments.repository.PostRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class PostViewModel @Inject constructor(private val postRepository: PostRepository): ViewModel() {
+    fun getAllPosts(): LiveData<List<PostListItem>>{
+        return postRepository.getAllPosts()
+    }
+
+    fun makeGetRequest(){
+        postRepository.makeGetRequest()
+    }
+}
