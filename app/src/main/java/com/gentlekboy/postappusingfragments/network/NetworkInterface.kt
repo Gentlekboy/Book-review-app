832f83f0cd178883df9fac532d710ebb81277089
gentlekboy <gentlekboy@gmail.com>
2021-09-20 01:26:17 +0100

    set up interface with endpoint and network request type

---
+++ app/src/main/java/com/gentlekboy/postappusingfragments/network/NetworkInterface.kt
@@ -0,0 +1,11 @@
+package com.gentlekboy.postappusingfragments.network
+
+import com.gentlekboy.postappusingfragments.model.PostList
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NetworkInterface {
+    //This function specifies the request type and endpoint
+    @GET("posts")
+    fun getPostsFromApi(): Call<PostList>
+}
