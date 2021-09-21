dbd8c55fe12e230e26561c04cfbdada741de054e
gentlekboy <gentlekboy@gmail.com>
2021-09-21 03:15:00 +0100

    added @AndroidEntryPoint annotation

--- app/src/main/java/com/gentlekboy/postappusingfragments/ui/MainActivity.kt
+++ app/src/main/java/com/gentlekboy/postappusingfragments/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.gentlekboy.postappusingfragments
+package com.gentlekboy.postappusingfragments.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gentlekboy.postappusingfragments.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
