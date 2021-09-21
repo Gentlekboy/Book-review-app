ee116ebb9fb2d016e12005d95c08db74f5728009
gentlekboy <gentlekboy@gmail.com>
2021-09-21 22:42:27 +0100

    created coment fragment

--- app/src/main/java/com/gentlekboy/postappusingfragments/ui/CommentsFragment.kt
+++ app/src/main/java/com/gentlekboy/postappusingfragments/ui/CommentsFragment.kt
@@ -5,16 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.gentlekboy.postappusingfragments.R
+import com.gentlekboy.postappusingfragments.adapter.CommentAdapter
+import com.gentlekboy.postappusingfragments.adapter.PostAdapter
+import com.gentlekboy.postappusingfragments.databinding.FragmentCommentsBinding
+import com.gentlekboy.postappusingfragments.databinding.FragmentPostsBinding
+import com.gentlekboy.postappusingfragments.viewModel.CommentViewModel
+import com.gentlekboy.postappusingfragments.viewModel.PostViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CommentsFragment : Fragment() {
+    private lateinit var commentAdapter: CommentAdapter
+    private var _binding: FragmentCommentsBinding? = null
+    private val binding get() = _binding!!
+    private val commentViewModel: CommentViewModel by viewModels()
+    val args: CommentsFragmentArgs by navArgs()
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comments, container, false)
+    ): View {
+        _binding = FragmentCommentsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val postId = args.postId
+
+        initRecyclerViewAdapter()
+        observeViewModel(postId)
+    }
+
+    private fun observeViewModel(postId: String) {
+        commentViewModel.makeGetRequest(postId)
+        commentViewModel.getAllComments().observe(viewLifecycleOwner, {
+            commentAdapter.differ.submitList(it)
+        })
+    }
+
+    private fun initRecyclerViewAdapter() {
+        commentAdapter = CommentAdapter()
+        binding.recyclerviewInComment.adapter = commentAdapter
+        binding.recyclerviewInComment.setHasFixedSize(true)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
