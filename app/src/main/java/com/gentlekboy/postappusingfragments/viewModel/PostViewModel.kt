4405edbe59b81a724dce9449ed4d3a9a5055e57b
gentlekboy <gentlekboy@gmail.com>
2021-09-21 22:43:33 +0100

    refactored import

--- app/src/main/java/com/gentlekboy/postappusingfragments/viewModel/PostViewModel.kt
+++ app/src/main/java/com/gentlekboy/postappusingfragments/viewModel/PostViewModel.kt
@@ -2,7 +2,7 @@ package com.gentlekboy.postappusingfragments.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.gentlekboy.postappusingfragments.model.PostListItem
+import com.gentlekboy.postappusingfragments.model.posts.PostListItem
 import com.gentlekboy.postappusingfragments.repository.PostRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
