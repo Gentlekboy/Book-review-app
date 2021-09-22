7bccc2f5c844c2a9360025fbb5ac9417c39e271e
gentlekboy <gentlekboy@gmail.com>
2021-09-22 08:36:10 +0100

    parcelized data class

--- app/src/main/java/com/gentlekboy/postappusingfragments/model/posts/PostListItem.kt
+++ app/src/main/java/com/gentlekboy/postappusingfragments/model/posts/PostListItem.kt
@@ -1,13 +1,16 @@
 package com.gentlekboy.postappusingfragments.model.posts
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "posts")
+@Parcelize
 data class PostListItem(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val body: String,
     val title: String,
     val userId: Int
-)
+) : Parcelable
