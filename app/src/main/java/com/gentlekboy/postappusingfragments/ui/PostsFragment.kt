06ebe308abc82dde2eadc9612525b190da25a0ec
gentlekboy <gentlekboy@gmail.com>
2021-09-22 08:37:32 +0100

    implemented adding of new post to database

--- app/src/main/java/com/gentlekboy/postappusingfragments/ui/PostsFragment.kt
+++ app/src/main/java/com/gentlekboy/postappusingfragments/ui/PostsFragment.kt
@@ -1,15 +1,15 @@
 package com.gentlekboy.postappusingfragments.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.gentlekboy.postappusingfragments.adapter.PostAdapter
 import com.gentlekboy.postappusingfragments.databinding.FragmentPostsBinding
-import com.gentlekboy.postappusingfragments.model.posts.PostListItem
 import com.gentlekboy.postappusingfragments.utils.ClickPostInterface
 import com.gentlekboy.postappusingfragments.viewModel.PostViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +20,7 @@ class PostsFragment : Fragment(), ClickPostInterface {
     private var _binding: FragmentPostsBinding? = null
     private val binding get() = _binding!!
     private val postViewModel: PostViewModel by viewModels()
+    private val args: PostsFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +33,22 @@ class PostsFragment : Fragment(), ClickPostInterface {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.floatingActionButton.setOnClickListener {
+            val action = PostsFragmentDirections.actionPostsFragmentToAddPostFragment(postAdapter.differ.currentList.size)
+            findNavController().navigate(action)
+        }
+
         initRecyclerViewAdapter()
+        addNewBook()
         observeViewModel()
+    }
+
+    private fun addNewBook() {
+        val newBook = args.postListItem
+
+        if (newBook != null){
+            postViewModel.addNewPost(newBook)
+        }
     }
 
     private fun observeViewModel() {
@@ -49,13 +64,13 @@ class PostsFragment : Fragment(), ClickPostInterface {
         binding.postRecyclerview.setHasFixedSize(true)
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-
     override fun navigateToCommentsActivity(position: Int, postId: Int) {
         val action = PostsFragmentDirections.actionPostsFragmentToCommentsFragment(postId.toString())
         findNavController().navigate(action)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
